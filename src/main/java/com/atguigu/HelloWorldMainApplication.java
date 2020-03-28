e292dc3de4245e9fa4c54492b81d8feb6f8b2463
100288 <spjiang@aliyun.com>
2020-03-29 00:24:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        // spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+}
